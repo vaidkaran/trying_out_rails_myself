f376c185f4aaac4f979b24b3af06be2cfbd1c5cb
Karan Vaid <vaidkaran89@gmail.com>
2014-11-02 12:13:44 +0530

    fully implemented comments except deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article) 
+   
+# Even this code works (just like we created the article)
+#    @comment = @article.comments.new(comment_params)
+#    if @comment.save
+#    redirect_to article_path(@article)   
+#    else
+#      puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#    end
+  end
+
+private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
 end
